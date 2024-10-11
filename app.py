2dc70f721ad7ee4a2ebe5368fa9eb5a6d071b433
aifreak00 <prathameshettam2001@gmail.com>
2024-10-11 17:25:16 +0530

    Your commit message

---
+++ app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from lazypredict import LazyClassifier
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+import uvicorn
+
+app=FastAPI()
+
+@app.get("/train")
+async def classification():
+    data=load_breast_cancer()
+    x=data.data
+    y=data.target
+    x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.5)
+    clf=LazyClassifier()
+    models,predictions=clf.fit(x_train,x_test,y_train,y_test)
+    models_dict=models.to_dict(orient="index")
+    return models_dict
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
